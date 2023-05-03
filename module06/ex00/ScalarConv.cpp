64fe7d2ef76a6841e8610cf1539e10bd8e415418
Ali Boudjemai <alboudje@z4r3p3.42lyon.fr>
2023-05-03 16:52:18 +0200

    Added more parsing

--- module06/ex00/ScalarConv.cpp
+++ module06/ex00/ScalarConv.cpp
@@ -6,7 +6,7 @@
 /*   By: alboudje <alboudje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 12:27:28 by alboudje          #+#    #+#             */
-/*   Updated: 2023/05/03 16:10:33 by alboudje         ###   ########.fr       */
+/*   Updated: 2023/05/03 16:51:20 by alboudje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	ScalarConverter::parsing(const std::string &literal)
 	std::cout << "is int " << isInt(literal) << std::endl; 
 	std::cout << "is char " << isChar(literal) << std::endl; 
 	std::cout << "is float " << isFloat(literal) << std::endl; 
+	std::cout << "is double " << isDouble(literal) << std::endl; 
 }
 
 void	ScalarConverter::convert(const std::string &literal)
@@ -40,6 +41,15 @@ bool	ScalarConverter::isInt(const std::string &literal)
 	int					num;
 	std::stringstream	ss;
 
+	if (literal == "+inf" || literal == "-inf")
+		return (true);
+	for (size_t i = 0; i < literal.size(); i++)
+	{
+		if (i == 0 && literal[i] == '-')
+			i++;
+		if (literal[i] < '0' || literal[i] > '9')
+			return (false);
+	}
 	ss << literal;
 	ss >> num;
 	return (!ss.fail());
@@ -50,14 +60,35 @@ bool	ScalarConverter::isFloat(const std::string &literal)
 	float				num;
 	std::stringstream	ss;
 
+
+	if (literal == "+inff" || literal == "-inff")
+		return (true);
 	ss << literal;
 	ss >> num;
-	if (literal[literal.size() - 1] == 'f' && ss.fail() == false)
+	if (literal[literal.size() - 1] == 'f'  && literal.find(".") != std::string::npos && ss.fail() == false)
 		return (true);
 	return (false);
 }
 
-/*bool	ScalarConverter::isDouble(const std::string &literal)
+bool	ScalarConverter::isDouble(const std::string &literal)
 {
-	(void)literal;
-}*/
+	float				num;
+	std::stringstream	ss;
+	bool				point;
+
+	point = false;
+	ss << literal;
+	ss >> num;
+	for (size_t i = 0; i < literal.size(); i++)
+	{
+		if ((i == 0 && literal[i] == '-'))
+			i++;
+		if (literal[i] == '.')
+			point = true;
+		if ((literal[i] < '0' || literal[i] > '9') && point)
+			return (false);
+	}
+	if (point && ss.fail() == false)
+		return (true);
+	return (false);
+}
--- module06/ex00/main.cpp
+++ module06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: alboudje <alboudje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:00:11 by alboudje          #+#    #+#             */
-/*   Updated: 2023/05/03 16:13:36 by alboudje         ###   ########.fr       */
+/*   Updated: 2023/05/03 16:22:19 by alboudje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@
 int	main(int argc, char **argv)
 {
 	(void)argc;
-	ScalarConverter::convert(argv[1]);	
+	ScalarConverter::convert(argv[1]);
 	return (0);
 }
