2f8eff0b0b3af01ea95b5d1214f0bbf6b6838a34
Ali Boudjemai <alboudje@z4r5p1.42lyon.fr>
2023-05-05 12:00:10 +0200

    Finished ex02

--- module06/ex02/Base.cpp
+++ module06/ex02/Base.cpp
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Base.hpp                                           :+:      :+:    :+:   */
+/*   Base.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alboudje <alboudje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/03 12:07:51 by alboudje          #+#    #+#             */
-/*   Updated: 2023/05/05 11:38:54 by alboudje         ###   ########.fr       */
+/*   Created: 2023/05/05 11:47:12 by alboudje          #+#    #+#             */
+/*   Updated: 2023/05/05 11:47:31 by alboudje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BASE_HPP
-#define BASE_HPP
+#include "Base.hpp"
 
-#include <iostream>
-
-class Base
+Base::~Base()
 {
-	public:
-		virtual ~Base();
-};
-
-class A : public Base{};
-class B : public Base{};
-class C : public Base{};
-
-#endif //BASE_HPP
+	
+}
--- module06/ex02/main.cpp
+++ module06/ex02/main.cpp
@@ -6,26 +6,83 @@
 /*   By: alboudje <alboudje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:00:11 by alboudje          #+#    #+#             */
-/*   Updated: 2023/05/05 11:22:58 by alboudje         ###   ########.fr       */
+/*   Updated: 2023/05/05 11:58:13 by alboudje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Serializer.hpp"
+#include "Base.hpp"
+#include <ctime>
+#include <cstdlib>
+
+Base	*generate(void)
+{
+	int	i;
+
+	i = std::rand() % 3;
+	switch (i)
+	{
+	case 0:
+		return (new A());
+		break;
+	case 1:
+		return (new B());
+		break;
+	case 2:
+		return (new C());
+		break;
+	default:
+		return (NULL);
+		break;
+	}
+}
+
+void	identify(Base *b)
+{
+	std::cout << "Pointer identify" << std::endl;
+	if (dynamic_cast<A *>(b) != NULL)
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B *>(b) != NULL)
+		std::cout << "B" << std::endl;
+	else
+		std::cout << "C" << std::endl;
+}
+
+void	identify(Base &b)
+{
+	std::cout << "Reference identify" << std::endl;
+	try
+	{
+		A &a = dynamic_cast<A&>(b);
+		static_cast<void>(a);
+		std::cout << "A" << std::endl;
+	}
+	catch (...)
+	{}
+	try
+	{
+		B &bt = dynamic_cast<B&>(b);
+		static_cast<void>(bt);
+		std::cout << "B" << std::endl;
+	}
+	catch (...)
+	{}
+	try
+	{
+		C &c = dynamic_cast<C&>(b);
+		static_cast<void>(c);
+		std::cout << "C" << std::endl;
+	}
+	catch (...)
+	{}
+}
 
 int	main(void)
 {
-	Data		*d = new (std::nothrow)Data;
-	Data		*d2;
-	uintptr_t	p;
+	Base	*rbase;
 
-	if (!d)
-		return (1);
-	d->i = 15;
-	std::cout << "i: " << d->i << std::endl;
-	p = Serializer::serialize(d);
-	d2 = Serializer::deserialize(p);
-	std::cout << "i: " << d->i << std::endl;
-	std::cout << "i2: " << d2->i << std::endl;
-	delete d;
-	return (0);
+	srand(std::time(0));
+	rbase = generate();
+	identify(rbase);
+	identify(*rbase);
+	delete rbase;
 }
